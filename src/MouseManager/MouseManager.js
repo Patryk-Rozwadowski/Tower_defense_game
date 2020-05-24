9b23e2709c43a7b07acfc3e7f00c0f38c2cf433f
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-05-24 23:43:47 +0200

    Fixed gameMap

--- src/MouseManager/MouseManager.js
+++ src/MouseManager/MouseManager.js
@@ -11,45 +11,35 @@ export class MouseManager {
   }
 
   init() {
-    this.mouseMoveHandler(this.drawMousePosition())
+    this.mouseMoveHandler()
+    this.mouseClickHandler()
   }
 
   mouseMoveHandler() {
     const self = this;
     this.canvas.addEventListener('mousemove', (e) => {
+      self.ctx.clearRect(0,0,self.canvas.width, self.canvas.height);
       self.normalizationCursorPosition(e)
       self.drawMousePosition()
     })
   }
 
-  // mouseClickHandler(handler, a) {
-  //   const self = this;
-  //   this.canvas.addEventListener('click', e => {
-  //
-  //     //norm
-  //     handler(e);
-  //     return function(handlerd) {
-  //       handlerd(e);
-  //     }
-  //   });
-  // }
-
-  drawMousePosition() {
-    this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
-    this.ctx.globalAlpha = 0.5;
-    this.ctx.fillRect(this.getMousePos().x * this.cellSize,this.getMousePos().y*this.cellSize, this.cellSize, this.cellSize);
+  mouseClickHandler() {
+    const self = this;
+    this.canvas.addEventListener('click', e => {
+      self.normalizationCursorPosition(e);
+      return self.getMousePosCanvas();
+    });
   }
 
-  // placeTurret() {
-  //   canvas.addEventListener('click', e => {
-  //     ctx.save();
-  //     ctx.globalAlpha = 0.5;
-  //     ctx.fillRect(a.getMousePos().x,a.getMousePos().y, cellWidth, cellHeight);
-  //     ctx.restore();
-  //   })
-  // } self.ctx.clearRect(0,0,self.canvas.width, self.canvas.height);
-  //         self.ctx.globalAlpha = 0.5;
-  //         self.ctx.fillRect(self.getMousePos().x * this.cellSize,self.getMousePos().y*self.cellSize, self.cellSize, self.cellSize);
+  drawMousePosition() {
+    this.ctx.globalAlpha = 0.5;
+    this.ctx.fillRect(this.getMousePosPerTile().x * this.cellSize,this.getMousePosPerTile().y*this.cellSize, this.cellSize, this.cellSize);
+  }
+
+  placeTurret() {
+    this.ctx.fillRect(this.getMousePosPerTile().x * this.cellSize,this.getMousePosPerTile().y*this.cellSize, this.cellSize, this.cellSize);
+  }
 
   normalizationCursorPosition(e) {
     let rect = this.canvas.getBoundingClientRect();
@@ -59,10 +49,16 @@ export class MouseManager {
     this.cellY = ~~(this.y/this.cellSize);
   }
 
-  getMousePos() {
+  getMousePosPerTile() {
     return {
       x: this.cellX,
       y: this.cellY
     }
+  }
+
+  getMousePosCanvas() {
+    let x = Math.ceil(this.x / 10) * 10;
+    let y = Math.ceil(this.y / 10) * 10;
+    return [x,y]
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,41 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 window.onload = () => {
-    const cols = 15;
-    const rows = 15;
+    const cols = 25;
+    const rows = 25;
 
     const cellWidth = 25;
     const cellHeight = 25;
-    const cellSize = 25;
-    ctx.canvas.width  = cols * cellWidth;
-    ctx.canvas.height = cellHeight *rows ;
+    const cellSize = 20;
+    ctx.canvas.width  = cols * cellSize;
+    ctx.canvas.height = rows * cellSize;
 
     const gameMap = [];
 
     const mouseManager = new MouseManager(canvas, ctx, cellSize);
+
+
+    let setMap;
+
+    let y = 0;
+    let x = 0;
+    for (let i = 0; i < rows; i++) {
+        gameMap[i] = [];
+
+        if( i > 0 ) y += cellHeight;
+
+        for (let j = 0; j < cols; j++) {
+            if(j > 0) x+= cellSize;
+
+            setMap = createTile(x,y);
+            gameMap[i][j] = setMap;
+
+            // ctx.beginPath()
+            // ctx.moveTo(x,y)
+            // ctx.lineTo(x,y)
+            // ctx.stroke();
+        }
+    }
 
     const tile = {
         id: Math.random(),
@@ -26,39 +49,26 @@ window.onload = () => {
     }
     mouseManager.init();
 
-    console.log(gameMap);
-
     function drawMap() {
-        // let y = -1;
-        // for (let i = 0; i < rows; i++) {
-        //     let x = -1;
-        //     y += cellHeight;
-        //     for (let j = 0; j < cols; j++) {
-        //         x += cellWidth;
-        //         debugger
-        //         gameMap.push(createTile(x,y));
-        //
-        //     }
-        // }
-        //
-        //
-
-
         ctx.globalAlpha = 0.5;
         ctx.lineWidth = 2;
 
-        for(var i = 0; i < ctx.canvas.width; i++) {
-            ctx.beginPath();
-            ctx.moveTo(i*cellSize, 0);
-            ctx.lineTo(i*cellSize, canvas.height);
-            ctx.stroke();
-        }
-        for(var i = 0; i < ctx.canvas.height; i++) {
-            ctx.beginPath();
-            ctx.moveTo(0, i*cellHeight);
-            ctx.lineTo(canvas.width, i*cellSize);
-            ctx.stroke();
-        }
+        // for(let vector of gameMap) {
+        //    // console.log(vector)
+        // }
+
+        // for(var i = 0; i < ctx.canvas.width; i++) {
+        //     ctx.beginPath();
+        //     ctx.moveTo(i*cellSize, 0);
+        //     //ctx.lineTo(i*cellSize, canvas.height);
+        //     ctx.stroke();
+        // }
+        // for(var i = 0; i < ctx.canvas.height; i++) {
+        //     ctx.beginPath();
+        //     ctx.moveTo(0, i*cellHeight);
+        //     //ctx.lineTo(canvas.width, i*cellSize);
+        //     ctx.stroke();
+        // }
 
     }
     function createTile(x, y) {
@@ -68,19 +78,25 @@ window.onload = () => {
                 vector: [x, y],
                 color: '#222',
                 tower: false
-
         }
     }
 
-    function placeTurret(x,y) {
-        console.log(`Place turret: ${x} ${y}`);
+    function placeTurret(vector) {
+        // ctx.beginPath();
+        // ctx.rect()
+        gameMap.find(el => el.vector === vector ? console.log('sdf ') : console.log('nope'))
+        console.log(`Place turret: ${vector.x} ${vector.y}`);
+
+        console.log(gameMap)
     }
+    canvas.addEventListener('click', () => placeTurret(mouseManager.getMousePosPerTile()))
 
     function draw() {
         drawMap()
+
         requestAnimationFrame(draw);
     }
 
 
-    draw();
+    //draw();
 };
