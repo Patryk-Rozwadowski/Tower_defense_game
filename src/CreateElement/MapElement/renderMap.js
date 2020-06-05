e0e61aaa3cebde7144e47d0786254451ac448b6b
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-06-05 23:30:37 +0200

    Walls folder moved into MapElement

--- src/CreateElement/MapElement/renderMap.js
+++ src/CreateElement/MapElement/renderMap.js
@@ -4,9 +4,9 @@ import {
   isLeftWall,
   isRightWall,
   isUpperWall,
-} from '../Walls/WallsUtils';
+} from './Walls/WallsUtils';
 
-import { createWallTile } from '../Walls/CreateWallTile';
+import { createWallTile } from './Walls/CreateWallTile';
 
 export function renderMap(ctx, gameMap, rows, cols, cellSize) {
   let y = 0;
