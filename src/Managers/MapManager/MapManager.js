31ca6a326a6f739b2b02bcf41437b67e29cef387
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-06-06 22:27:11 +0200

    Optimized and reorganized map render, add MapManager

---
+++ src/Managers/MapManager/MapManager.js
@@ -0,0 +1,70 @@
+import {
+  isBottomWall,
+  isLeftWall,
+  isRightWall,
+  isUpperWall,
+} from '../../CreateElement/MapElement/Walls/wallsUtils';
+import { createWallTile } from '../../CreateElement/MapElement/Walls/createWallTile';
+import { createTerrainTile } from '../../CreateElement/MapElement/createTerrainTile';
+
+export class MapManager {
+  constructor(canvas, ctx, cellSize) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.cols = 15;
+    this.rows = 15;
+
+    this.x = 0;
+    this.y = 0;
+
+    this.cellSize = cellSize;
+    this.gameMap = [];
+  }
+
+  renderMap() {
+    this.x = 0;
+    this.y = 0;
+    for (let i = 0; i <= this.rows; i++) {
+      this.x = 0;
+      this._createAndAddRow(i);
+
+      if (i > 0) this.y += this.cellSize;
+      for (let j = 0; j <= this.cols; j++) {
+        if (j > 0) this.x += this.cellSize;
+
+        // WALLS
+        this._createAndAddWalls(i, j, this.x, this.y);
+
+        // TERRAIN
+        this._createAndAddTerrain(i, j);
+
+        let xVec = this.gameMap[i][j].vector[0];
+        let yVec = this.gameMap[i][j].vector[1];
+
+        this.ctx.fillStyle = this.gameMap[i][j].color;
+        this.ctx.fillRect(xVec, yVec, this.cellSize, this.cellSize);
+      }
+    }
+  }
+
+  _createAndAddRow(i) {
+    this.gameMap[i] = [];
+  }
+
+  _createAndAddWalls(i, j) {
+    if (
+      isUpperWall(j) ||
+      isLeftWall(i) ||
+      isRightWall(i + 1, this.cols) ||
+      isBottomWall(j + 1, this.rows)
+    ) {
+      this.gameMap[i][j] = createWallTile(this.x, this.y);
+    }
+  }
+
+  _createAndAddTerrain(i, j) {
+    if (!this.gameMap[i][j]) {
+      this.gameMap[i][j] = createTerrainTile(this.x, this.y);
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import { MouseManager } from './Managers/MouseManager/MouseManager';
 import { renderMap } from './CreateElement/MapElement/renderMap';
 import { TurretsManager } from './Managers/TurretManager/TurretManager';
+import { MapManager } from './Managers/MapManager/MapManager';
 
 window.onload = () => {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
+
   const cols = 15;
   const rows = 15;
   const cellSize = 25;
-
-  const gameMap = [];
 
   ctx.canvas.width = cols * cellSize;
   ctx.canvas.height = rows * cellSize;
 
   const mouseManager = new MouseManager(canvas, ctx, cellSize);
   const turretsManager = new TurretsManager(canvas, ctx, cellSize);
+  const mapManager = new MapManager(canvas, ctx, cellSize);
 
   mouseManager.mouseClickHandler(() =>
     turretsManager.placeTurret(mouseManager.getMousePosPerTile())
   );
 
   function draw() {
-    renderMap(ctx, gameMap, rows, cols, cellSize);
+    mapManager.renderMap();
     mouseManager.drawMousePosition();
     turretsManager.renderTurrets();
     requestAnimationFrame(draw);
